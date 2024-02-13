ac7ca65aee042687a76d1e24e32850b664bd752a
Magdalena <mkrolikowska01@poczta.fm>
2024-02-13 13:58:48 +0100

    optimized imports

--- src/main/java/project/gui/App.java
+++ src/main/java/project/gui/App.java
@@ -1,17 +1,7 @@
 package project.gui;
-import javafx.geometry.Insets;
-import javafx.scene.paint.Color;
-import project.*;
+
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.geometry.HPos;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.*;
 import javafx.stage.Stage;
-import project.statictics.StatisticsPanel;
 
 public class App extends Application {
 
